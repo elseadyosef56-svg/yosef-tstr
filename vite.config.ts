3a72d878ff3fb726e49625864aaeb9b37159dce4
elseadyosef56-svg <elseadyosef56@gmail.com>
2026-01-18 20:21:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,3 +21,11 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/yosef-tst/", // اسم الريبو هنا مهم
+});
+
